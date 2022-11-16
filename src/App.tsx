eff98b0267b22a79159f35ce9341884ca5535008
Mauricio Brito <maubot01@gmail.com>
2022-11-15 23:42:40 -0300

    feat: initial unpkg plugin added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
-import { useState } from 'react'
+import * as esbuild from 'esbuild-wasm'
+import { useState, useEffect, useRef } from 'react'
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
 
 const App = () => {
   const [input, setInput] = useState('')
   const [code, setCode] = useState('')
+  const serviceRef = useRef<any>()
 
-  const onClick = () => {
-    console.log(input)
+  const startService = async () => {
+    serviceRef.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    })
   }
+
+  const onClick = async () => {
+    if (!serviceRef.current) return
+    const result = await serviceRef.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+    })
+    // console.log(result)
+    setCode(result.outputFiles[0].text)  
+  }
+
+  useEffect(() => {
+    startService()
+  }, [])
 
   return (
     <div>
@@ -14,7 +36,9 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre></pre>
+      <pre>
+        {code}
+      </pre>
     </div>
   )
 }
